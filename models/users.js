70d2a315613e73b3857afe358a6b9fd2b533d03b
Kevin Smith <kevin@kvnsmith.com>
2018-05-05 12:57:57 -0600

    files n junk

---
+++ models/users.js
@@ -0,0 +1,17 @@
+module.exports =function (sequelize, DataTypes){
+	var User = sequelize.define("User", {
+		user_name: {
+			type: DataTypes.STRING,
+			validate: {
+				notNull: true
+			}
+		},
+		userEmail: {
+			type: DataTypes.STRING,
+			validate:{
+			isEmail: true,           // checks for email format (foo@bar.com)
+			}
+		},
+		password: DataTypes.STRING
+	})
+};
