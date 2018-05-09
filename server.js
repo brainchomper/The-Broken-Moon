40ef85a70d5165413f6881b2a3541af33afffe4f
Kevin Smith <kevin@kvnsmith.com>
2018-05-08 20:15:58 -0600

    working server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 
 require("./routes/html-routes.js")(app);
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
