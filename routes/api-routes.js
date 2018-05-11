e25ef70d2bba4ec3dfa3b12859bfb7c708915b3a
Kevin Smith <kevin@kvnsmith.com>
2018-05-10 19:36:50 -0600

    user api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,5 +19,14 @@ module.exports = function(app) {
 		});
 	});
 
+	app.post("/api/user", function(req, res){
+		db.users.add({
+			user_id: req.body.user_id,
+			user_name: req.body.user_name,
+			user_email: req.body.user_email,
+			user_photo: req.body.user_photo
+		}
+		)
+	})
 	// end
 };
