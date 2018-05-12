5a3239b9cd3a16ba04dc0a28aad58c4aa82f6e01
Csgraser <csgraser@gmail.com>
2018-05-12 15:15:11 -0600

    got the charactercreatorapp.js to function properly

--- public/javascript/characterCreatorApp.js
+++ public/javascript/characterCreatorApp.js
@@ -1,40 +1,30 @@
-
-// list of variables that we'll need for the character build
-var hp = 0;
-var str = 0;
-var agi = 0;
-var int = 0;
-var charImage = '';
-
-
-$('#submitCharacter').on("click", function(event){
+$('#submitCharacter').on("click", function (event) {
 	// preventing the default
 	console.log("the button was clicked");
 	event.preventDefault();
 	// make a new object for the api call
 
 	//get the values from the page and login info
-	var charClass = $("#characterClass option:selected").text();
-console.log(charClass)
-evaluateClass(charClass);
-
+	var charClass = $("#class option:selected").val();
+	console.log(charClass)
+	var charAttr = evaluateClass(charClass);
 	var newChar = {
 		// we need to access this from the local storage 
 		user_owner: 1,
 
 		character_class: charClass,
 		character_name1: $("#charName1").val(),
-		character_name2:  $("#charName2").val(),
-		character_img: charImage,
+		character_name2: $("#charName2").val(),
+		character_img: charAttr.charImage,
 		level: 1,
-		hp: hp,
-		str: str,
-		agi: agi,
-		int: int,
+		hp: charAttr.hp,
+		str: charAttr.str,
+		agi: charAttr.agi,
+		int: charAttr.int,
 		exp: 0,
 		loot: ""
 	};
-console.log('newchar', newChar);
+	console.log('newchar', newChar);
 	$.ajax({
 		url: "/api/characters",
 		method: "POST",
@@ -42,48 +32,57 @@ console.log('newchar', newChar);
 		dataType: "json"
 	}
 	)
-.then(console.log("stuff"))
+		.then(console.log("stuff"))
 });
 
 
 // uses a switchase on the classInput to change different attributes
-function evaluateClass(classInput){
-	console.log("evaluating")
-	switch(classInput){
+function evaluateClass(classInput) {
+	console.log("evaluating" + classInput);
+	switch (classInput) {
 		case "Wizard": {
-			hp = 10;
-			str = 5;
-			agi = 10;
-			int = 15;
-			charImage: "/assets/characterimages/wizard.png";
+			return {
+				
+				hp: 10,
+				str: 5,
+				agi: 10,
+				int: 15,
+				charImage: "/assets/characterimages/wizard.png"
+			};
 
 
-			console.log("HP, wizard" , hp)
+			console.log("HP, wizard", hp)
 		};
 
-		break;
-		case "Rogue":{
-			hp = 10;
-			str = 10;
-			agi =15;
-			int = 5;
-			charImage: "/assets/characterimages/rogue.png"
-		}
-		console.log("HP, rogue" , hp)
+			break;
+		case "Rogue": {
+			return {
 
-		break;
+				hp: 10,
+				str: 10,
+				agi: 15,
+				int: 5,
+				charImage: "/assets/characterimages/rogue.png"
+			}
+		}
+			console.log("HP, rogue", hp)
+
+			break;
 		case "Warrior": {
-			hp =10;
-			str =15;
-			agi = 5;
-			int = 10;
-			charImage: "/assets/characterimages/warrior.png"
-			console.log("HP," , hp)
+			return {
+
+				hp: 10,
+				str: 15,
+				agi: 5,
+				int: 10,
+				charImage: "/assets/characterimages/warrior.png"
+			}
+			console.log("HP,", hp)
 
 		}
-		break;
+			break;
 		default:
-		console.log('dafuq')
+			console.log('dafuq')
 	}
 
 }
