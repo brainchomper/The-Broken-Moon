dfcb59db4c65faa16b67b869c335df474ddce035
Kevin Smith <kevin@kvnsmith.com>
2018-05-12 11:42:01 -0600

    login cookies

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -24,5 +24,34 @@ module.exports = function (app) {
 		});
 	});
 
+	// user creation
+
+	app.post("/api/user", function (req, res) {
+		db.User.findOrCreate({
+			where: {
+				id_token: req.body.id_token
+			},
+			defaults: {
+				user_name: req.body.user_name,
+				user_email: req.body.user_email,
+				user_photo: req.body.user_photo
+			}
+
+		}).then(function (dbuser) {
+			res.json(dbuser)
+		})
+	});
+
+
+	app.get("/api/user/info", function (req, res) {
+		db.User.find({
+			where: {
+				id_token: req.query.id_token
+			}
+		}).then(function (dbuser) {
+				res.json(dbuser)
+			});
+	});
+
 	// end
 };
