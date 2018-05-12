61a3c71da2a7dc81359c94f4ad2ff4c5cb97c62e
Kevin Smith <kevin@kvnsmith.com>
2018-05-12 11:17:38 -0600

    login and cookie

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -23,40 +23,6 @@ module.exports = function (app) {
 			res.render()
 		});
 	});
-<<<<<<< HEAD
-	// user creation
-
-	app.post("/api/user", function (req, res) {
-		db.User.findOrCreate({
-			where: {
-				id_token: req.body.id_token
-			},
-			defaults: {
-				user_name: req.body.user_name,
-				user_email: req.body.user_email,
-				user_photo: req.body.user_photo
-			}
-
-		}).then(function (dbuser) {
-			res.json(dbuser)
-		})
-	});
-
-
-	app.get("/api/user/info", function (req, res) {
-		db.User.find({
-			where: {
-				id_token: req.query.id_token
-			}
-		}).then(function (dbuser) {
-			console.log("&&&&&&&", dbuser)
-				res.json(dbuser)
-			});
-	});
-
-	//end
-=======
 
 	// end
->>>>>>> 8bdb64da527e25fc660e20186cc573cf8898af73
 };
