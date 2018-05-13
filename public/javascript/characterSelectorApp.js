5b8561ca4cd98ba6838ee54823cfa1e9c4e666f9
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-05-13 14:57:31 -0600

    most of the character selector logic is running but some CSS needed to polish it. Moving on to merge w/ Casey's file and work on the fight screen next.

--- public/javascript/characterSelectorApp.js
+++ public/javascript/characterSelectorApp.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
+// hide the fight button
+	$("#fightBtn").hide();
 
 	var charID ;
 	$(".pickCharacter").on("click", function(event){
 		event.preventDefault();
-		$(this).parent().addClass("selectedCharacter");
-		charID =  $(this).id.val;
-		console.log("charID", CharID);
-		// make a cookie
-		setCookie("selectedCharacter", charID);
-	})
+		// remove selected Character 
+		// $(".selectedCharacter").removeClass("selectedCharacter");
+		// add the selected character class to the card 
+		$(this).parent().parent().addClass("selectedCharacter");
+		charID =  $(this).id;
+		console.log("charID", charID);
+		//show the fight button
+		$("#fightBtn").show();
+		
+	});
 
-})
+	$("#fightBtn").on("click", function(event){
+		event.preventDefault();
+		setCookie("selectedCharacter", charID);
+
+		// run the apiquery which will serve up the fight screen;
+	});
 
 function setCookie(cname, cvalue) {
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	document.cookie = cname + "=" + cvalue + ";" + " path=/";
 };
+// end page functions
+});
