5804270dce119124e9a9fd9d9b0777c01e6c467d
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-05-14 23:57:11 -0600

    all the circular button logic on the fightscreen is now working but we need to add in the actual fight logic to the button click

--- public/javascript/fightscreenApp.js
+++ public/javascript/fightscreenApp.js
@@ -1,59 +1,117 @@
-$(document).ready(function(){
-// load the value of the character
-// var medBtn = $("#medMonster").val();
-// var easyBtn = $("#easyMonster").val();
-// var hardBtn = $("#hardMonster").val();
-$(".monsterSearchBtn").on("click", function(event){
-	event.preventDefault();
-	console.log("this, " , this);
-	APIcall(this);;
-})
+	// placeholder for the values we will get from the monster later
+	var monsterScoreArr;
 
-	// end of the page function
-});
+	//placeholder variable for the challenge rating of the fight
+	var challengeRating;
 
-function APIcall(obj){
-	// get the value
-	var addOn = obj.value;
-	// concatenate the value of the button into a query url
-	var queryURL = "/api/find" + addOn;
-// run the query and then run the visualize function on the results
-$.ajax({
-	url: queryURL,
-	method: "GET",
+	// globally scoped pseudo-react pieces:
 
-})
-.then(function(results){
-	visualize(results)
-})
-	
-}
+	function buttonMaker(obj) {
+		console.log("obj", obj)
+		return $("<button>")
+			.addClass("monsterSearchBtn")
+			.attr("value", obj)
+			.text("Fight " + obj + " Monster");
+	}
 
-// function to help us read the cookies
-function getCookie(c_name) {
-	if (document.cookie.length > 0) {
+	$(document).ready(function () {
+
+		$('body').on("click", ".monsterSearchBtn", function (event) {
+			console.log("Click click click click click for the api")
+			event.preventDefault();
+			APIcall(this);;
+			challengeRating = this.value;
+			console.log("challengeRating", challengeRating);
+		})
+
+		$('body').on("click", ".startOverBtn", function (event) {
+			event.preventDefault();
+			console.log("clicking the start over btn")
+			// empty the fightRow
+			$("#fightRow").empty();
+			var eBtn = buttonMaker("Easy");
+			var mBtn = buttonMaker("Medium");
+			var hBtn = buttonMaker("Hard");
+			$("#monsterDiv")
+				.empty()
+				.append(
+					eBtn, mBtn, hBtn
+				)
+		});
+		$('body').on("click", ".fightStartBtn", function(){
+			// hey so we need to put the fight code in here yo
+		});
+
+		// end of the page function
+	});
+
+	function APIcall(obj) {
+		// get the value
+		var addOn = obj.value;
+		// set the challenge rating to the value
+		challengeRating = addOn
+		// concatenate the value of the button into a query url
+		var queryURL = "/api/find" + addOn;
+		// run the query and then run the visualize function on the results
+		$.ajax({
+				url: queryURL,
+				method: "GET",
+			})
+			.then(function (results) {
+				visualize(results)
+			})
+
+	}
+
+	// function to help us read the cookies
+	function getCookie(c_name) {
+		if (document.cookie.length > 0) {
 			c_start = document.cookie.indexOf(c_name + "=");
 			if (c_start != -1) {
-					c_start = c_start + c_name.length + 1;
-					c_end = document.cookie.indexOf(";", c_start);
-					if (c_end == -1) {
-							c_end = document.cookie.length;
-					}
-					return unescape(document.cookie.substring(c_start, c_end));
+				c_start = c_start + c_name.length + 1;
+				c_end = document.cookie.indexOf(";", c_start);
+				if (c_end == -1) {
+					c_end = document.cookie.length;
+				}
+				return unescape(document.cookie.substring(c_start, c_end));
 			}
+		}
+		return "";
 	}
-	return "";
-}
 
-function visualize(obj) {
-	var header = $("<h4>").addClass("card-title")
+	function visualize(obj) {
+		var cbody = $("<div>").addClass("card-body");
+		var newImg = $("<img>").attr("src", obj.photo).addClass("monsterImg");
+		var header = $("<h4>").addClass("card-title").text(obj.name);
+		// update the scoreArr that we will use later for the fight functions
+		monsterScoreArr = obj.scores;
+		// make a new card but with custom class
+		var updateDiv =
+			$("<div>")
+			.addClass("card monsterCard")
+			// fill the insides
+			.append(
+				cbody
+				.append(newImg, header)
+			)
+		// move the buttons out and replace w/ the monster info
+		$("#monsterDiv").html(updateDiv);
+		// update the fightrow with a button
+		var fightBtn = $("<button>")
+			.addClass("fightStartBtn")
+			.text("Start the fight!");
+		var newChallenge = $("<button>")
+			.addClass("startOverBtn")
+			.text("Change Difficulty Rating");
+		console.log("farther down", challengeRating)
+		var newMonster = $("<button>")
+			.addClass("monsterSearchBtn")
+			.attr("value", challengeRating)
+			.text("Random Monster (Same Difficulty)");
 
-	// make a new card but with custom class
-	$("<div>")
-	.addClass("card monsterCard")
-	// fill the insides
-	.html(
-		
-	)
-}
 
+		$("#fightRow")
+			.empty()
+			.append(fightBtn, newChallenge, newMonster)
+
+	}
