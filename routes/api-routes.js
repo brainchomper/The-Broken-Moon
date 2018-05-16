9c86ae65e8d2395f8938ce589ffebd76db993dee
t-alex-b <sunnoperatic@aol.com>
2018-05-15 18:19:40 -0600

    tentative update to 'put' request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,56 +1,33 @@
 var db = require("../models");
 
-
-console.log('----- db.User.', db.User);
-
-module.exports = function (app) {
-	app.post("/api/characters", function (req, res) {
-
+module.exports = function(app) {
+	app.post("/api/characters", function(req, res){
 		db.Character.create(req.body)
-			.then(function (dbCharacter) {
-				res.redirect("/menu")
-			})
+		.then(function(dbCharacter){
+			res.redirect("/menu")
+		})
 	})
 
-	app.get("/api/characters", function (req, res) {
+	app.get("/api/characters", function(req, res){
 		db.Character.findAll({
-		where:{
-		user_owner: 1}
+			where: {
+				user_owner: req.body.id
+			}
 		})
 		.then(function(dbCharacter){
-			console.log(dbCharacter)
-			res.render("character_selector" , dbCharacter);
-			res.render()
+			res.render(dbCharacter);
 		});
-	});
-  
-	// user creation
+	});	
 
-	app.post("/api/user", function (req, res) {
-		db.User.findOrCreate({
+	app.put("/api/characters", function(req,res){
+		db.Character.findAll({
 			where: {
-				id_token: req.body.id_token
-			},
-			defaults: {
-				user_name: req.body.user_name,
-				user_email: req.body.user_email,
-				user_photo: req.body.user_photo
+				user_owner: req.body.id
 			}
-
-		}).then(function (dbuser) {
-			res.json(dbuser)
 		})
-	});
-
-
-	app.get("/api/user/info", function (req, res) {
-		db.User.find({
-			where: {
-				id_token: req.query.id_token
-			}
-		}).then(function (dbuser) {
-				res.json(dbuser)
-			});
-	});
+		.then(function(updateCharacter){
+			res.send(updateCharacter);
+		})
+	})
 	// end
 };
